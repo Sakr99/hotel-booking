1072c63e9d0104d608e22ce5f1ae18470b73bd6a
Abdelrahman Sakr <abdelrahmansakr199@gmail.com>
2025-04-06 23:42:59 +0200

    countriesOption-componenet
    build CountriesOption section

--- src/Components/CountriesOption.jsx
+++ src/Components/CountriesOption.jsx
@@ -1,12 +1,47 @@
-
+import imageBG from '../Assets/imageBG.jpg'
+import imageSM from '../Assets/imageSM.png'
 const CountriesOption = () => {
   return (
-    <div className="CountriesOption max-w-fit h-screen m-7 hidden sm:block">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem omnis
-      repudiandae aliquid ullam fugit sunt optio? Quas consequatur beatae
-      incidunt. Libero error est architecto eaque! Illo eveniet dolorum
-      aspernatur laborum.
-    </div>
+    <main className="hidden sm:block  p-20 ">
+      <div
+        className="w-full h-screen relative  flex flex-col  px-10 rounded-2xl bg-cover bg-center overflow-x-auto"
+        style={{ backgroundImage: `url(${imageBG})` }}
+      >
+        <p className=" w-3/4 p-10 text-6xl ">
+          Popular vacation rental destinations
+          <p className=" text-xl font-medium ">
+            Need some inspiration for your next getaway? Find the perfect
+            oceanfront, cozy, or luxurious hotel in one of these top
+            destinations.
+          </p>
+        </p>
+        <div className="absolute bottom-20 left-10 gap-6 w-full  flex ">
+          <div className="relative">
+            <img src={imageSM} className=" rounded-2xl" alt="imageSM" />
+            <p className="absolute top-20 left-4 text-lg font-bold"> Florida</p>
+          </div>
+          <div className="relative">
+            <img src={imageSM} className=" rounded-2xl" alt="imageSM" />
+            <p className="absolute top-20 left-4 text-lg font-bold"> Florida</p>
+          </div>
+          <div className="relative">
+            <img src={imageSM} className=" rounded-2xl" alt="imageSM" />
+            <p className="absolute top-20 left-4 text-lg font-bold"> Florida</p>
+          </div>
+          <div className="relative">
+            <img src={imageSM} className=" rounded-2xl" alt="imageSM" />
+            <p className="absolute top-20 left-4 text-lg font-bold"> Florida</p>
+          </div>
+          <div className="relative">
+            <img src={imageSM} className=" rounded-2xl" alt="imageSM" />
+            <p className="absolute top-20 left-4 text-lg font-bold ">
+              {" "}
+              Florida
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
